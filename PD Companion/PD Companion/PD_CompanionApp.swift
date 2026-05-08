71b19ee15ed2e28cc9de5e78bf43c249177806a4
Bhav Bhasin <bhavdeep.bhasin@gmail.com>
2026-05-07 23:07:01 -0700

    Add Xcode project with iOS and watchOS targets
    iOS target: HealthKit, Motion & Fitness, Background Modes (fetch + processing)
    watchOS target: HealthKit, Background Modes (workout processing, physical therapy)
    Updated .gitignore for Xcode user-specific files

---
+++ PD Companion/PD Companion/PD_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  PD_CompanionApp.swift
+//  PD Companion
+//
+//  Created by Bhav Bhasin on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct PD_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
